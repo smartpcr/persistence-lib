fdef64133cf14d0786e0e104d57fc30a56dd1eef
Xiaodong Li <xiaodoli@microsoft.com>
2025-08-10 03:20:38 -0700

    refactor audit

---
+++ Providers/SQLite/Audit/IAuditProvider.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="IAuditProvider.cs" company="Microsoft Corp.">
+//     Copyright (c) Microsoft Corp. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Microsoft.AzureStack.Services.Update.Common.Persistence.Provider.SQLite.Audit
+{
+    public class IAuditProvider
+    {
+        
+    }
+}
